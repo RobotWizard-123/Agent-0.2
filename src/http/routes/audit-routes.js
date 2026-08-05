55bac735630cc6520cfbe30f4066ad86a7c16ccf
RobotWizard-123 <18502293736@163.com>
2026-08-05 10:16:35 +0800

    Delete src directory

--- src/http/routes/audit-routes.js
+++
@@ -1,6 +0,0 @@
-export function registerAuditRoutes(router, { repository }) {
-  router.add("GET", "/api/audit", () => {
-    const state = repository.read();
-    return { state_version: state.version, items: [...state.audit].reverse() };
-  });
-}
